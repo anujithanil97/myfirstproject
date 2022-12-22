f8550bf3632248afab21565e433c270dd4d977dc
Anujith A <anujithanil24@gmail.com>
2022-12-22 11:00:06 +0530

    firstchange

---
+++ app.js
@@ -0,0 +1,17 @@
+// function convertFarenheit(celsius){
+//     let temp = celsius
+//     let result =(temp*9/5)+32
+//     console.log("farenheit  value:" , result)
+  
+// }
+
+
+// function convertCelsius(farenheit){
+//     let temp = farenheit
+//     let result = (temp-32)*5/9
+//     console.log("celsius value:" , result)
+  
+// }
+// convertFarenheit(100);
+// convertCelsius(108)
+
